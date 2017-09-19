8409c199ea59367aa6ae1d600e5a6edccbcf0a76
林千里 <lincheney@gmail.com>
2017-09-20 03:48:07 +1000

    load custom functions based on env var

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,24 @@
 extern crate lazy_static;
 extern crate libc;
 
+use std::env;
+use std::ffi::{OsStr, OsString, CStr, CString};
+use std::os::unix::ffi::{OsStringExt, OsStrExt};
 use std::sync::{Once, ONCE_INIT};
 
 mod readline {
-    type rl_command_func_t = extern fn(isize, isize) -> isize;
+    pub type rl_command_func_t = extern fn(isize, isize) -> isize;
 
     #[link(name = "readline")]
     extern {
-        pub fn rl_add_funmap_entry(name: *const u8, function: rl_command_func_t) -> isize;
+        fn rl_add_funmap_entry(name: *const u8, function: rl_command_func_t) -> isize;
+    }
+
+    pub fn add_function(name: &[u8], function: rl_command_func_t) {
+        let name = ::CString::new(name).unwrap();
+        unsafe{ rl_add_funmap_entry(name.as_ptr() as *const u8, function) };
+        // readline now owns the string
+        ::std::mem::forget(name);
     }
 
     lazy_static! {
@@ -24,17 +34,43 @@ mod readline {
     }
 }
 
+fn add_function(name: &[u8]) -> Result<(), OsString> {
+    let name_cstr = CString::new(name).unwrap();
+
+    let lib = unsafe{ ::libc::dlopen(name_cstr.as_ptr() as *const i8, ::libc::RTLD_NOW) };
+    if ! lib.is_null() {
+        let func = unsafe{ ::libc::dlsym(lib, b"rl_custom_function\0".as_ptr() as *const i8) };
+        if ! func.is_null() {
+            let func = unsafe{ ::std::mem::transmute(func) };
+            // use filename as command name
+            let command = std::path::Path::new(OsStr::from_bytes(name)).file_stem().unwrap();
+            readline::add_function(command.as_bytes(), func);
+            return Ok(());
+        }
+    }
+
+    let error = unsafe{ ::libc::dlerror() };
+    let error = unsafe{ CStr::from_ptr(error) }.to_bytes();
+    let error = OsStr::from_bytes(error).to_os_string();
+    return Err(error)
+}
+
 static INIT: Once = ONCE_INIT;
 
 #[no_mangle]
 pub extern fn rl_initialize_funmap() {
     INIT.call_once(|| {
-        unsafe{ readline::rl_add_funmap_entry(b"hello\0" as *const u8, custom_command) };
-    });
-    unsafe{ readline::RL_INITIALIZE_FUNMAP() }
-}
+        if let Some(value) = env::var_os("READLINE_CUSTOM_FUNCTION_LIBS") {
+            let value = value.into_vec();
 
-extern fn custom_command(count: isize, key: isize) -> isize {
-    println!("{:?}", 123);
-    0
+            for part in value.split(|c| *c == b':') {
+                if let Err(e) = add_function(part) {
+                    let part = OsStr::from_bytes(part);
+                    eprintln!("Error loading {:?}: {:?}", part, e);
+                }
+            }
+        }
+    });
+
+    unsafe{ readline::RL_INITIALIZE_FUNMAP() }
 }
