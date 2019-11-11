663d604f42d3b44a5c19dde892ccd3c8cec362ff
林千里 <lincheney@gmail.com>
2019-11-11 21:08:07 +1100

    expose dynlib errors

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,20 @@ use std::ffi::OsStr;
 use std::os::unix::ffi::{OsStringExt, OsStrExt};
 use std::sync::Once;
 
+type DynlibResult<T> = Result<T, &'static str>;
+
+macro_rules! dump_error {
+    ($result:expr, $default:expr) => {
+        match $result {
+            Ok(x) => x,
+            Err(e) => { eprintln!("{}", e); return $default },
+        }
+    }
+}
+
 macro_rules! dynlib_call {
     ($func:ident($($args:expr),*)) => {{
-        let ptr = {
-            use libc::$func;
-            $func($($args),*)
-        };
+        let ptr = libc::$func($($args),*);
         if ptr.is_null() {
             let error = libc::dlerror();
             if error.is_null() {
@@ -24,11 +32,6 @@ macro_rules! dynlib_call {
             Ok(ptr)
         }
     }}
-}
-
-macro_rules! dlopen {
-    ($name:expr) => { dlopen!($name, libc::RTLD_LAZY) };
-    ($name:expr, $flags:expr) => { dynlib_call!(dlopen($name.as_ptr() as _, $flags)) };
 }
 
 macro_rules! dlsym {
@@ -47,59 +50,46 @@ macro_rules! dlsym {
 mod readline {
     pub use self::lib::rl_initialize_funmap;
 
-    pub fn add_function(name: &[u8], function: lib::rl_command_func_t) {
+    pub fn add_function(name: &[u8], function: lib::rl_command_func_t) -> ::DynlibResult<()> {
         let name = std::ffi::CString::new(name).unwrap();
-        unsafe{ lib::rl_add_funmap_entry(name.as_ptr(), function) };
+        unsafe{ (*lib::rl_add_funmap_entry)?(name.as_ptr(), function) };
         // readline now owns the string
         std::mem::forget(name);
+        Ok(())
     }
 
     #[allow(non_upper_case_globals, non_camel_case_types)]
     mod lib {
-        use std::marker::PhantomData;
         pub type rl_command_func_t = extern fn(isize, isize) -> isize;
 
-        pub struct Pointer<T>(usize, PhantomData<T>);
-        impl<T> Pointer<T> {
-            pub fn new(ptr: *mut T)    -> Self { Self(ptr as _, PhantomData) }
-            pub fn ptr(&self)        -> *mut T { self.0 as *mut T }
-            pub unsafe fn set(&self, value: T) { *self.ptr() = value; }
-        }
-
-        lazy_static! {
-            pub static ref libreadline: Pointer<libc::c_void> = Pointer::new(unsafe {
-                if dlsym!(libc::RTLD_DEFAULT, "rl_initialize", usize).is_ok() {
-                    libc::RTLD_DEFAULT
-                } else {
-                    dlopen!(b"libreadline.so\0").unwrap()
-                }
-            });
-        }
         macro_rules! readline_lookup {
             ($name:ident: $type:ty) => {
-                readline_lookup!($name: $type, libreadline.ptr());
+                readline_lookup!($name: $type; libc::RTLD_DEFAULT);
             };
-            ($name:ident: $type:ty, $handle:expr) => {
-                lazy_static! { pub static ref $name: $type = unsafe{ dlsym!($handle, stringify!($name)) }.unwrap(); }
+            ($name:ident: $type:ty; $handle:expr) => {
+                lazy_static! {
+                    pub static ref $name: ::DynlibResult<$type> = unsafe {
+                        dlsym!($handle, stringify!($name)).or_else(|_|
+                            dynlib_call!(dlopen(b"libreadline.so\0".as_ptr() as _, libc::RTLD_NOLOAD | libc::RTLD_LAZY))
+                            .and_then(|lib| dlsym!(lib, stringify!($name)))
+                        )};
+                }
             }
         }
 
-        readline_lookup!(rl_initialize_funmap: unsafe extern fn(),
-            if libreadline.ptr() == libc::RTLD_DEFAULT { libc::RTLD_NEXT } else { libreadline.ptr() });
+        readline_lookup!(rl_initialize_funmap: unsafe extern fn(); libc::RTLD_NEXT);
         readline_lookup!(rl_add_funmap_entry:  unsafe extern fn(*const i8, rl_command_func_t) -> isize);
     }
 }
 
-fn add_function(name: &[u8]) -> Result<(), &str> {
+fn add_function(name: &[u8]) -> DynlibResult<()> {
     let name_cstr = std::ffi::CString::new(name).unwrap();
 
-    unsafe{ dlopen!(name_cstr) }.and_then(|lib|
-        unsafe{ dlsym!(lib, "rl_custom_function") }.map(|func| {
-            // use filename as command name
-            let command = std::path::Path::new(OsStr::from_bytes(name)).file_stem().unwrap();
-            readline::add_function(command.as_bytes(), func);
-        })
-    )
+    let lib = unsafe{ dynlib_call!(dlopen(name_cstr.as_ptr() as _, libc::RTLD_LAZY)) }?;
+    let func = unsafe{ dlsym!(lib, "rl_custom_function") }?;
+    // use filename as command name
+    let command = std::path::Path::new(OsStr::from_bytes(name)).file_stem().unwrap();
+    readline::add_function(command.as_bytes(), func)
 }
 
 static INIT: Once = Once::new();
@@ -119,5 +109,5 @@ pub extern fn rl_initialize_funmap() {
         }
     });
 
-    unsafe{ readline::rl_initialize_funmap() }
+    unsafe{ dump_error!(*readline::rl_initialize_funmap, ())() }
 }
